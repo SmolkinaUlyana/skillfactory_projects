f143a5a345a88634254f981760c8f48300b2569b
smolkina.ulyana <smolkina.ulyana@gmail.com>
2020-05-25 02:26:54 +0600

    second commit

---
+++ module_0/guessnumber.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count+=1
+        predict = np.random.randint(1,101) # предполагаемое число
+        if number == predict: 
+            return(count) # выход из цикла, если угадали
+        
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1,101)
+    while number != predict:
+        count+=1
+        if number > predict: 
+            predict += 1
+        elif number < predict: 
+            predict -= 1
+    return(count) # выход из цикла, если угадали
+
+
+def game_core_v3(number):
+    count=1
+    predict = np.random.randint(1,101)
+    
+    ls=list(range(1,101))
+    
+    while number != predict:
+        count+=1
+        if predict < number:
+            ls=list(filter(lambda x: x !=predict, ls[ls.index(predict):]))   
+            predict=ls[ls.index(max(ls))//2]
+        elif predict > number: 
+            ls=list(filter(lambda x: x !=predict,ls[:ls.index(predict)]))
+            predict=ls[ls.index(max(ls))//2]        
+    return(count) # выход из цикла, если угадали  
+
+
+      
+def score_game(number):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
